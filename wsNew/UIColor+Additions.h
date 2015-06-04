1b796a7f3cd6840fbca10b444105937d9bc5ac9e
Senro <840787626@qq.com>
2015-06-04 13:45:09 +0800

    release 1.0.0

---
+++ wsNew/UIColor+Additions.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+Additions.h
+//  WJTR
+//
+//  Created by TYRBL on 15/5/16.
+//  Copyright (c) 2015年 Senro Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Additions)
+
+
++ (UIColor *)colorWithHexString: (NSString *)color;
+
+@end
